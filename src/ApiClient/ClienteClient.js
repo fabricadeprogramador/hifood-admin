1894ad7334c0c5f8b92b03f361cfd8b2205b1864
TheNisTh <denis_thielke@hotmail.com>
2021-09-09 01:58:34 -0400

    Atualizando Clientes

--- src/ApiClient/ClienteClient.js
+++ src/ApiClient/ClienteClient.js
@@ -1,8 +1,13 @@
 const axios = require("axios");
-const host = "http://localhost:3000/cliente";
+const hostClientes = "http://localhost:3000/cliente";
+const hostCompras = "http://localhost:3000/cliente/compra";
 
 export default {
-  buscarTodos() {
-    return axios.get(host).then((resposta) => resposta);
-  },
+	buscarTodos() {
+		return axios.get(hostClientes).then((resposta) => resposta);
+	},
+	buscarCompras(cpf) {
+		let hostCPF = hostCompras + "/" + cpf;
+		return axios.get(hostCPF).then((resposta) => resposta);
+	},
 };
